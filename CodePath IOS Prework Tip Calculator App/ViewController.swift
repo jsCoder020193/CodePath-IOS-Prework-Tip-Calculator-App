e30eda2cf1feec32b8d4b1b1307869db7d9e499c
Subba <Subba@gmail.com>
2021-01-31 20:18:54 -0600

    Initial Commit

---
+++ CodePath IOS Prework Tip Calculator App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodePath IOS Prework Tip Calculator App
+//
+//  Created by SubbaLakshmi on 1/31/21.
+//  Copyright © 2021 SubbaLakshmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CodePath IOS Prework Tip Calculator AppTests/CodePath_IOS_Prework_Tip_Calculator_AppTests.swift
@@ -0,0 +1,34 @@
+//
+//  CodePath_IOS_Prework_Tip_Calculator_AppTests.swift
+//  CodePath IOS Prework Tip Calculator AppTests
+//
+//  Created by SubbaLakshmi on 1/31/21.
+//  Copyright © 2021 SubbaLakshmi. All rights reserved.
+//
+
+import XCTest
+@testable import CodePath_IOS_Prework_Tip_Calculator_App
+
+class CodePath_IOS_Prework_Tip_Calculator_AppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
